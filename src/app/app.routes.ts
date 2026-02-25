80f81d6788021e48ed4cd22936eb860a216ea8a3
rigoberto <rigoberto7567@gmail.com>
2026-02-24 21:46:06 -0600

    add page og the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DemoPageComponent } from './components/demo-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'demo', component: DemoPageComponent }
+];
